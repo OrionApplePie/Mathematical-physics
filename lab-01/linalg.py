0bfa6ff8c78007ad12d4d793c51cd4a270955dba
Александр Тен <orionapplepie@gmail.com>
2019-03-17 15:01:25 +1000

    Правки стиля

--- lab-01/linalg.py
+++ lab-01/linalg.py
@@ -5,20 +5,20 @@ import numpy as np
 # например, на вход подается правая чась в виде array[[], [], []],
 # т.е. как двумерный Array, а не одномерный
 
+
 def gen_three_diag_by_3(elements, n):
-    """Генерация трехдиагональной матрицы размером n x n на основе 3 элементов."""
+    """Генерация трехдиагональной матрицы размером n x n
+    на основе 3 элементов."""
     left, diag, right = elements
     res = np.zeros((n, n))
-
     for i in range(n):
         for j in range(n):
             if i == j:
                 res[i, j] = diag
                 if j - 1 >= 0:
-                    res[i, j - 1] = left 
+                    res[i, j - 1] = left
                 if j + 1 < n:
                     res[i, j + 1] = right
-    
     return res
 
 
@@ -53,7 +53,6 @@ def seidel1(A, b, eps):
     """Реализация метода Зейделя из википедии."""
     n = len(A)
     x = [.0 for i in range(n)]
-
     converge = False
     while not converge:
         x_new = np.copy(x)
@@ -61,10 +60,10 @@ def seidel1(A, b, eps):
             s1 = sum(A[i][j] * x_new[j] for j in range(i))
             s2 = sum(A[i][j] * x[j] for j in range(i + 1, n))
             x_new[i] = (b[i] - s1 - s2) / A[i][i]
-
-        converge = math.sqrt(sum((x_new[i] - x[i]) ** 2 for i in range(n))) <= eps
+        converge = math.sqrt(
+            sum((x_new[i] - x[i]) ** 2 for i in range(n))
+        ) <= eps
         x = x_new
-
     return x
 
 
@@ -79,17 +78,18 @@ def seidel(A, f, eps, norm):
     m = len(f)    
     x_prev = np.zeros(m)
     x_next = np.zeros(m)
-
     while(True):
-    # основной цикл итераций
-        for i in range(m): # для каждой компоненты вектора решения (x)
+        # основной цикл итераций
+        for i in range(m):  # для каждой компоненты вектора решения (x)
             sum1 = 0
             sum2 = 0
-            # Суммирование от 0 до i-1 -го элемента, или по всем эл-м от начального (0) которые меньше i-го
+            # Суммирование от 0 до i-1 -го элемента,
+            # или по всем эл-м от начального (0) которые меньше i-го
             # range(i) дает элементы от 0 до i-1 (!)
             for j in range(i):
                 sum1 += A[i, j]*x_next[j]
-            # Суммирование от i+1-го до m-1-го элемента, или по всем эл-м от начального (i+i) которые больше i-го
+            # Суммирование от i+1-го до m-1-го элемента,
+            # или по всем эл-м от начального (i+i) которые больше i-го
             # range(i+1, m) дает элементы от i+1 до m-1-го (!)
             for j in range(i+1, m):
                 sum2 += A[i, j]*x_prev[j]
@@ -98,11 +98,12 @@ def seidel(A, f, eps, norm):
 
         # if norm1(x_next - x_prev) < eps:
         if norm(x_next, x_prev) <= eps:
-            # если по норме разность векторов решений на предыдущем и текущем шагах меньше эпс, то выход из главного цикла
+            # если по норме разность векторов решений на предыдущем и
+            # текущем шагах меньше эпс, то выход из главного цикла
             break
-        # если нет - то текущее решение становиться предыдущим для новой итерации
+        # если нет - то текущее решение становиться предыдущим
+        # для новой итерации
         x_prev = np.copy(x_next)
-        
     return x_next
 
 
@@ -116,7 +117,6 @@ def TDMA(A, d):
     n = len(d)
     # список для решения
     x = [0] * n
-    
     # прямой ход:
     # проходимся по строкам матрицы А
     for i in range(n - 1):
@@ -124,17 +124,14 @@ def TDMA(A, d):
         alpha.append(
             -c / (b + a*alpha[i])
         )
-                
         beta.append(
             (d[i][0] - a * beta[i]) / (b + a * alpha[i])
         )
     # вычисление последнего компонента решения (xm - см. теорию)
     # n-1го т.к. нумерация в массивах numpy идет с 0
     a, b = A[n - 1, n - 2], A[n - 1, n - 1]
-    x[n - 1] = ( d[n - 1][0] - a * beta[n - 1] ) / ( b + a * alpha[n - 1] )
-    
-    # прямой ход
+    x[n - 1] = (d[n - 1][0] - a * beta[n - 1]) / (b + a * alpha[n - 1])
+    # обратный ход
     for i in reversed(range(n - 1)):
-        x[i] = alpha[i + 1] * x[i + 1] +  beta[i + 1]
-
+        x[i] = alpha[i + 1] * x[i + 1] + beta[i + 1]
     return x
--- lab-01/main.py
+++ lab-01/main.py
@@ -12,7 +12,8 @@ from linalg import (
 
 
 def insert_boundary_values(u):
-    # add values in 0 and 1, просто вствляем известные значения в крайних узлаах
+    # add values in 0 and 1,
+    # просто вствляем известные значения в крайних узлаах
     n = len(u)
     u = np.insert(
         u,
@@ -30,7 +31,6 @@ def insert_boundary_values(u):
 
 
 if __name__ == "__main__":
-    
     # domain
     a = 0.0
     b = 1.0
@@ -38,49 +38,40 @@ if __name__ == "__main__":
     # h - step, n - nodes
     # print('Enter nodes number: ')
     h = 0.01
-    n = 99 # inner nodes
-     
+    n = 99  # inner nodes
     # coefficient of stiffness matrix
     a_left = a_right = (h / 6.) - (1. / h)
     a_center = (2. / h) + (2.0*h) / 3.0
-
     # print('n= {}, h={}, a_left={}'.format(n, h, a_left))
-    
     # generating of stiffnes matrix
     A = diags(
         diagonals=[a_left, a_center, a_right],
         offsets=[-1, 0, 1],
         shape=(n, n)
     ).toarray()
-    
     b = np.full(
         shape=(n, 1),
         fill_value=h
     )
     # print(A)
     # print(b)
-
     u_k_np = np.linalg.solve(A, b)
-    
     u_k = seidel(
         A,
         b,
         eps,
         norm1
     )
-
     u_k2 = seidel(
         A,
         b,
         eps,
         norm_euclid
     )
-    
     u_k3 = TDMA(
         A,
         b
     )
-    
     # with angsquare morm
     u_k4 = seidel(
         A,
@@ -88,21 +79,17 @@ if __name__ == "__main__":
         eps,
         norm_avg_square
     )
-    # print(np.transpose(u_k).tolist())    
-
+    # print(np.transpose(u_k).tolist())
     u_k_np = insert_boundary_values(u_k_np)
-    u_k = insert_boundary_values(u_k) 
-    u_k2 = insert_boundary_values(u_k2) 
+    u_k = insert_boundary_values(u_k)
+    u_k2 = insert_boundary_values(u_k2)
     u_k3 = insert_boundary_values(u_k3)
     u_k4 = insert_boundary_values(u_k4)
-    
     # Setka 0 to 1 по всем узлам
     x_h = np.array(
         [i*h for i in range(n + 2)]
     )
-    
     # print(x_h)
-
     plt.grid()
     plt.xlabel('x')
     plt.ylabel('y')
@@ -111,12 +98,11 @@ if __name__ == "__main__":
     plt.plot(x_h, u_k, 'g', label="Норма макс")
     plt.plot(x_h, u_k2, 'b-', label='Евклидова норма')
     plt.plot(x_h, u_k3, 'y--', label="Метод прогонки")
-    
+
     plt.plot(
         x_h, u_k4,
         'c--',
         label="Среднеквадратическая норма"
     )
-    
     plt.legend()
     plt.show()
