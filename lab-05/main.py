d4af5e920adf5a917699c7f760926a723f3d2b94
Ten Alexander <orionapplepie@gmail.com>
2019-12-16 13:02:12 +1000

    Добавлено формирование матрицы жесткости и правой части

--- lab-05/main.py
+++ lab-05/main.py
@@ -15,9 +15,18 @@ from mpl_toolkits.mplot3d import Axes3D
 from linalg import norm_euclid, seidel
 
 EPS = 1E-5  # точность для м. Зейделя
-NODES = 4 # количество узлов по стороне, 99 узлов считает очень долго
-NN = NODES * NODES
-h = 1.0 / NODES
+N = 4  # количество узлов по стороне
+NODES = N * N  # всего узлов
+h = 1.0 / N
+
+E = 70  # aluminium
+Mu = 0.34
+Mu_1 = Mu / (1 - Mu)
+E_1 = E / (1 - Mu*Mu)
+G = G1 = E / (2*(1 + Mu))
+
+P1 = 100
+P2 = -100
 
 # константы мапперы со значениями интегралов различных комбинаций
 
@@ -33,7 +42,7 @@ vals_f = {
 
 # dphi_ik/dx1 * dphi_i/dx1
 vals_dx1_dx1 = {
-    # внутренние (неграничные) пары узлов 
+    # внутренние (неграничные) пары узлов
     'self_inner': 2.0,
 
     'up': 0.0,
@@ -66,7 +75,7 @@ vals_dx1_dx1 = {
 
 # dphi_ik/dx2 * dphi_i/dx1
 vals_dx2_dx1 = {
-    # внутренние (неграничные) пары узлов 
+    # внутренние (неграничные) пары узлов
     'self_inner': -1,
 
     'up': 0.5,
@@ -99,7 +108,7 @@ vals_dx2_dx1 = {
 
 # dphi_ik/dx2 * dphi_i/dx1
 vals_dx2_dx2 = {
-    # внутренние (неграничные) пары узлов 
+    # внутренние (неграничные) пары узлов
     'self_inner': 2,
 
     'up': -1,
@@ -132,7 +141,7 @@ vals_dx2_dx2 = {
 
 # dphi_ik/dx2 * dphi_i/dx1
 vals_dx1_dx2 = {
-    # внутренние (неграничные) пары узлов 
+    # внутренние (неграничные) пары узлов
     'self_inner': -1,
 
     'up': 0.5,
@@ -171,30 +180,30 @@ def get_type_of_node(node):
 
     res = ''
 
-    if (1 < i < NODES) and (1 < j < NODES):
-        res = 'inner' # неграничный внутренний узел
+    if (1 < i < N) and (1 < j < N):
+        res = 'inner'  # неграничный внутренний узел
 
-    elif (i == 1 and j == 1) or (i == NODES and j == NODES):
+    elif (i == 1 and j == 1) or (i == N and j == N):
         # левый нижний или верхний правый (2 кусочка треугольника)
         res = 'bound_angle_r_two'
 
-    elif (i == 1 and j == NODES) or (i == NODES and j == 1):
+    elif (i == 1 and j == N) or (i == N and j == 1):
         # левый верхний или нижний правый (1 кусочек треугольника)
         res = 'bound_angle_l_one'
 
-    elif (i == 1 and j != 1 and j != NODES):
+    elif (i == 1 and j != 1 and j != N):
         # на левой вертикали
         res = 'bound_vert_left'
 
-    elif (i == NODES and j != 1 and j != NODES):
+    elif (i == N and j != 1 and j != N):
         # на правой вертикили
         res = 'bound_vert_right'
 
-    elif (j == 1 and i != 1 and i != NODES):
+    elif (j == 1 and i != 1 and i != N):
         # нижняя горизонталь
         res = 'bound_horz_down'
 
-    elif (j == NODES and i != 1 and i != NODES):
+    elif (j == N and i != 1 and i != N):
         # верхняя горизонталь
         res = 'bound_horz_up'
 
@@ -235,16 +244,16 @@ def get_type_of_pair(node1, node2):
     elif ii == 1 and jj == 0:
         res = 'left'
     else:
-        res = 'none' # нет общих областей - интеграл равен 0
+        res = 'none'  # нет общих областей - интеграл равен 0
 
     # если на границе
     if (
         i1 == i2 == 1 or
-        i1 == i2 == NODES or
+        i1 == i2 == N or
         j1 == j2 == 1 or
-        j1 == j2 == NODES
+        j1 == j2 == N
     ):
-        if (ii == 0 and jj == 0): # если узел с самим собой
+        if (ii == 0 and jj == 0):  # если узел с самим собой
             if node1_type == 'bound_angle_l_one':
                 res = 'bound_self_triangle_one'
 
@@ -264,19 +273,19 @@ def get_type_of_pair(node1, node2):
                 res = 'bound_self_horz_down'
 
             else:
-                raise ValueError # TODO:fix me!!!
+                raise ValueError  # TODO:fix me!!!
 
         # проверка - если сосед
         elif abs(ii) == 1 and j1 == 1:
             res = 'bound_pair_horz_down'
 
-        elif abs(ii) == 1 and j1 == NODES:
+        elif abs(ii) == 1 and j1 == N:
             res = 'bound_pair_horz_up'
-        
+
         elif abs(jj) == 1 and i1 == 1:
             res = 'bound_pair_vert_left'
 
-        elif abs(jj) == 1 and i1 == NODES:
+        elif abs(jj) == 1 and i1 == N:
             res = 'bound_pair_vert_right'
 
         else:
@@ -291,8 +300,8 @@ def main():
     # обходим все узлы и сохраняем в список вместе с координатами,
     # координаты (j, i) - номер строки, номер колонки
     # обход низу вверх, слева направо
-    for row_num in range(1, NODES + 1):
-        for col_num in range(1, NODES + 1):
+    for row_num in range(1, N + 1):
+        for col_num in range(1, N + 1):
             nodes_list.append(
                 {
                     'node_num': node_num_count,
@@ -308,44 +317,95 @@ def main():
                f"type: {get_type_of_node(node)}\n"
                f"----------------------------"))
     # правая часть
-    f = np.array(
-        [0.0]*NN
-    )
+    f = np.zeros(2*NODES)
 
-    matrix = np.zeros((NN, NN))
+    # matrix = np.zeros((NN, NN))
     # обходим каждый узел с каждым и заполняем матрицу жесткости
-    # TODO: использовать симметричность матрицы
-    t1_1 = []
-    t2_1 = []
-    t1 = []
-    t2 = []
-    t3 = []
-    t4 = []
+    # TODO: использовать портрет матрицы
+
+    # По порядку как в уравнениях (3)
+    # первый индекс - это верхний у t^(i)
+    # в уравнеиях надо поправить чтобы шло t1, t2 - нужно для решения слау
+    #
+    t1_1 = np.zeros((NODES, NODES))
+    t2_2 = np.zeros((NODES, NODES))
+
+    t1_3 = np.zeros((NODES, NODES))
+    t2_4 = np.zeros((NODES, NODES))
+
+    t1_5 = np.zeros((NODES, NODES))
+    t2_6 = np.zeros((NODES, NODES))
+
+    t1_7 = np.zeros((NODES, NODES))
+    t2_8 = np.zeros((NODES, NODES))
 
     for node1, node2 in itertools.product(nodes_list, repeat=2):
-
         pair_type = get_type_of_pair(node1, node2)
 
-        t1.append(vals_dx1_dx1[pair_type])
-        t2.append(vals_dx2_dx1[pair_type])
-        t3.append(vals_dx2_dx2[pair_type])
-        t4.append(vals_dx1_dx2[pair_type])
+        i = node1['node_num']
+        j = node2['node_num']
+
+        node1_type = get_type_of_node(node1)
+        if node1_type == 'bound_vert_left':
+            f[i] = P1
+
+        if node1_type == 'bound_vert_right':
+            f[i] = P2
+
+        val_x1x1 = vals_dx1_dx1[pair_type]
+        val_x2x1 = vals_dx2_dx1[pair_type]
+        val_x2x2 = vals_dx2_dx2[pair_type]
+        val_x1x2 = vals_dx1_dx2[pair_type]
+
+        t1_1[i-1][j-1] = val_x1x1
+        t2_2[i-1][j-1] = val_x2x1
+
+        t1_3[i-1][j-1] = val_x2x2
+        t2_4[i-1][j-1] = val_x1x2
+
+        t1_5[i-1][j-1] = val_x1x2
+        t2_6[i-1][j-1] = val_x2x2
+
+        t1_7[i-1][j-1] = val_x2x1
+        t2_8[i-1][j-1] = val_x1x1
 
         print(
-            "node1 {0} --> node2 {1}, pair type: {2}, val={3}".format(
+            "node1 {0} --> node2 {1}, pair type: {2}".format(
                 node1['node_num'],
                 node2['node_num'],
-                pair_type,
-                a_ki
+                pair_type
             )
         )
-        # f[node1['node'] - 1] = vals_f[get_type_of_node(node1)]
 
-        # matrix[node1['node'] - 1][node2['node'] - 1] = a_ki
-    # print(f)
-    # print(vals_f)
+    factor1 = E_1 / (1 - Mu_1*Mu_1)
+
+    t1_1 *= factor1
+    t2_2 *= factor1*Mu_1
+    t1_3 *= G
+    t2_4 *= G
+
+    t1_5 *= factor1*Mu_1
+    t2_6 *= factor1
+    t1_7 *= G
+    t2_8 *= G
+
+    # приведение подобных
+    t1_1 += t1_3
+    t2_2 += t2_4
+
+    t1_5 += t1_7
+    t2_6 += t2_8
+
+    part_upper = np.concatenate((t1_1, t2_2), axis=1)
+    part_lower = np.concatenate((t1_5, t2_6), axis=1)
+
+    k_matrix = np.concatenate((part_upper, part_lower))
+    print(k_matrix)
+    print(k_matrix.shape)
+
     # решение слау встроенным решателем
-    # sol = np.linalg.solve(matrix, f)
+    sol = np.linalg.solve(k_matrix, f)
+
     # решение слау методом Зейделя
     # sol = seidel(
     #     matrix,
@@ -355,7 +415,7 @@ def main():
     # )
     # sol = np.around(sol, decimals=3)
 
-    # print(sol)
+    print(sol)
     # # из вектора решения делаем двумерный вектор
     # # размером NODES на NODES
     # Z = np.reshape(sol, (NODES, NODES))
